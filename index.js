ace6d341c630e80f869208edc6a7b5390bea137e
sifat-fahimul <mdsifatfeni@gmail.com>
2021-10-29 16:03:15 +0600

    heroku set running

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.fj83e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect()
+
+        const database = client.db('travellers')
+        const serviceCollection = database.collection('tourist');
+
+        //Get API
+        app.get('/package', async (req, res) => {
+            const cursor = serviceCollection.find({})
+            const package = await cursor.toArray()
+            res.send(package)
+        })
+
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+app.get('/', (req, res) => {
+    res.send('server running')
+})
+app.listen(port, () => {
+    console.log('running server on : ', port);
+})
